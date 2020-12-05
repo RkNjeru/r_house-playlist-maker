9fec26027cfda5515ead05c65748e1edd9dfd8f5
Roy Njeru <roynjeru24@gmail.com>
2020-12-04 21:13:47 -0500

    initial .net setup

---
+++ playlistApi/Models/HelloItem.cs
@@ -0,0 +1,11 @@
+namespace playlistApi.Models
+{
+    public class HelloItem
+    {
+        public long Id {get; set;}
+        public string Name {get; set;}
+
+        public bool IsOn {get; set;}
+
+    }
+}
